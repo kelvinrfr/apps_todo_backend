b4703022af69840f1eed67f858ac45cff32dc2bf
Kelvin Ferreira <10376375+k30v1n@users.noreply.github.com>
2023-09-23 16:58:06 -0400

    moving code to src physical folder

---
+++ src/TodoApp/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TodoApp.Data.Database;
+
+namespace TodoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<TodoAppDbContext>();
+                db.Migrate();
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
